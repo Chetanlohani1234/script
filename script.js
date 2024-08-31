e5df8a35fa1b4ec5eec5c45a935964142e9e1c70
chetanlohani4321 <chetanlohani310@gmail.com>
2024-08-31 12:01:13 +0530

    changes

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-// (function() {
-//     // Create a container for the chatbot
-//     const chatbotContainer = document.createElement('div');
-//     chatbotContainer.id = 'chatbot-container';
-//     document.body.appendChild(chatbotContainer);
-  
-//     // Create an iframe to load the chatbot
-//     const iframe = document.createElement('iframe');
-//     iframe.src = 'https://realchatbot-mauve.vercel.app/'; // URL of your chatbot
-//     iframe.width = '500px';
-//     iframe.height = '800px';
-//     iframe.style.cssText = 'border:none; position:fixed; bottom:20px; right:20px; border-radius:20px';
-  
-//     chatbotContainer.appendChild(iframe);
-  
-//     // Optional: Clean up the chatbot container when the page unloads
-//     window.addEventListener('unload', () => {
-//       document.body.removeChild(chatbotContainer);
-//     });
-// })();
 
 (function() {
   // Define the valid key for validation
@@ -42,7 +22,7 @@
 
     // Create an iframe to load the chatbot
     const iframe = document.createElement('iframe');
-    iframe.src = 'https://realchatbot-mauve.vercel.app/'; // URL of the chatbot
+    iframe.src = 'http://polymatric.com:8000/; // URL of the chatbot
     iframe.width = '500px';
     iframe.height = '800px';
     iframe.style.border = 'none';
